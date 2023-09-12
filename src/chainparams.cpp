a5a528765e6d4c675f7e4b0eb8de07ac48347c70
c4ei <c4ei.net@gmail.com>
2023-09-12 09:02:32 +0900

    chg ko

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -128,12 +128,12 @@ public:
 
         // Note that of those with the service bits flag, most only support a subset of possible options
         // vSeeds.emplace_back("seed-a.mtc4ei.loshan.co.uk", true);
-        // vSeeds.emplace_back("dnsseed.thrasher.io", true);
+        vSeeds.emplace_back("dnsseed.c4ei.net", true);
         // vSeeds.emplace_back("dnsseed.mtc4eitools.com", true);
         // vSeeds.emplace_back("dnsseed.mtc4eipool.org", true);
         // vSeeds.emplace_back("dnsseed.koin-project.com", false);
-        vSeeds.clear();
-        vSeeds.emplace_back("",true);
+        // vSeeds.clear();
+        // vSeeds.emplace_back("",true);
         // https://en.bitcoin.it/wiki/List_of_address_prefixes
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,50); // base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,48);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
